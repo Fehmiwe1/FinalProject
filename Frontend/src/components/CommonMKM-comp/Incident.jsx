e4fee2d14f4b98ab79dcc10a09a5520c27aa2731
Fehmiwe1 <fehmiwe1@gmail.com>
2025-05-09 13:00:55 +0300

    הוספת דף איורעים

--- Frontend/src/components/CommonMKM-comp/Incident.jsx
+++ Frontend/src/components/CommonMKM-comp/Incident.jsx
@@ -44,7 +44,7 @@ function Incident() {
         })
         .catch((error) => {
           console.error("שגיאה:", error);
-          setMsg("אירע שגיאה בעת מחיקת הדוח.");
+          setMsg("אירעה שגיאה בעת מחיקת הדוח.");
         });
     }
   };
